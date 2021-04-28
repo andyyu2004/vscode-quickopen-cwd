0a3a9350b79d26add9c784f98ae88bbf09269e38
Andy Yu <andyyu2004@gmail.com>
2021-04-28 21:32:51 +1200

    more or less working

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import { window, workspace } from "vscode";
+import * as path from "path";
 import * as vscode from "vscode";
 import * as cp from "child_process";
 import * as split from "split2";
@@ -13,27 +14,33 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function quickOpenCurrentDir() {
-  // const items = await getFiles();
-  // console.log(files);
+  const items = await getFiles();
+  console.log("getFiles returned", items);
   const quickPick = window.createQuickPick<File>();
   // quickPick.matchOnDescription = false;
   // quickPick.matchOnDetail = true;
   // quickPick.placeholder = "Search";
-  const items = [{ label: "werr" }];
-  console.log("populateSearch returned", items);
   if (!items) {
     return;
   }
+  quickPick.onDidAccept(async () => {
+    const file = quickPick.selectedItems[0];
+    const doc = await workspace.openTextDocument(file.path);
+    await window.showTextDocument(doc);
+  });
   quickPick.items = items;
   quickPick.show();
 }
 
-interface File extends vscode.QuickPickItem {}
+interface File extends vscode.QuickPickItem {
+  path: string;
+}
 
 function getFiles(): Promise<File[]> {
-  const cwd = workspace.workspaceFolders![0].uri.fsPath;
+  const currentFile = vscode.window.activeTextEditor!.document.uri.fsPath;
+  const cwd = path.join(currentFile, "..");
   return new Promise((resolve, reject) => {
-    const process = cp.spawn("fd", ["--type=f --exact-depth=1"], {
+    const process = cp.spawn("fd", ["--type=f", "--exact-depth=1"], {
       cwd,
     });
     const files: File[] = [];
@@ -42,7 +49,8 @@ function getFiles(): Promise<File[]> {
     stream.on("end", () => resolve(files));
     stream.on("error", () => reject("stream failed"));
     stream.on("data", (filename: string) => {
-      files.push({ label: filename });
+      console.log(filename);
+      files.push({ label: filename, path: path.join(cwd, filename) });
     });
   });
 }
